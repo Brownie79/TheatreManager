6c90adac031c7ef403968455d9099e9deaba8f6f
Wmei <wmei1@hawk.iit.edu>
2015-11-12 16:47:53 -0600

    completed create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -20,8 +20,8 @@ title varchar(32) PRIMARY KEY NOT NULL,
 movietype varchar(32),
 description varchar(2000));
 
-CREATE TABLE Star(
-movie varchar(43) PRIMARY KEY NOT NULL, -- fk to movie.title
+CREATE TABLE Star_(
+movie varchar(32) PRIMARY KEY NOT NULL, -- fk to movie.title
 name_ varchar(64) NOT NULL);	
 
 CREATE TABLE Director(
@@ -30,7 +30,7 @@ name_ varchar(64));
 
 CREATE TABLE Screen(
 theatre varchar(255) PRIMARY KEY NOT NULL, -- FK TO THEATRE.ADDRESS
-num_ int);
+screen int UNIQUE NOT NULL);
 
 CREATE TABLE ScreenCap (
 screen int PRIMARY KEY , -- fk to screen.num
@@ -68,7 +68,7 @@ company varchar(16)
 
 CREATE TABLE User_(
 email varchar(255) PRIMARY KEY NOT NULL,
-ccNum varchar(19) NOT NULL,
+ccNum varchar(19) UNIQUE NOT NULL,
 phone varchar(10) NOT NULL,
 f_name varchar(32) NOT NULL,
 l_name varchar(32) NOT NULL,
@@ -146,7 +146,7 @@ address varchar(255) NOT NULL,
 phone varchar(10) NOT NULL
 );
 
-CREATE TABLE Manager(
+CREATE TABLE Manager_(
 ssn varchar(9) PRIMARY KEY NOT NULL,
 name_ varchar(32) NOT NULL,
 location_ varchar(255) NOT NULL,				--fk to theatre.address
@@ -161,3 +161,93 @@ end_ date NOT NULL,
 type_ varchar(32) NOT NULL,							--fk to position.type
 location_ varchar(255) NOT NULL					--fk to theatre.address
 );
+
+--
+--Constraints
+
+ALTER TABLE Theatre
+ADD FOREIGN KEY (manager_) 
+REFERENCES manager_(ssn);
+
+ALTER TABLE MovieTimes
+ADD FOREIGN KEY (theatre) REFERENCES theatre(address);
+ALTER TABLE MovieTimes
+ADD FOREIGN KEY (screen) REFERENCES Screen(screen);
+ALTER TABLE MovieTimes
+ADD FOREIGN KEY (movie) REFERENCES movie(title);
+
+ALTER TABLE star_
+ADD FOREIGN KEY (movie) REFERENCES movie(title);
+
+ALTER TABLE director
+ADD FOREIGN KEY (movie) REFERENCES movie(title);
+
+ALTER TABLE screen 
+ADD FOREIGN KEY (theatre) REFERENCES theatre(address);
+
+ALTER TABLE screencap
+ADD FOREIGN KEY (screen) REFERENCES screen(screen);
+
+ALTER TABLE TheatreReviews
+ADD FOREIGN KEY (theatre) REFERENCES theatre(address);
+ALTER TABLE TheatreReviews
+ADD FOREIGN KEY (reviewer) REFERENCES registerInfo(username);
+
+ALTER TABLE moviereviews
+ADD FOREIGN KEY (movie) REFERENCES movie(title);
+ALTER TABLE moviereviews
+ADD FOREIGN KEY (review) REFERENCES registerInfo(username);
+
+ALTER TABLE creditcard
+ADD FOREIGN KEY (ccnum) REFERENCES user_(ccnum);
+
+ALTER TABLE user_ 
+ADD FOREIGN KEY (type_) REFERENCES membertype(type_);
+
+ALTER TABLE guestinfo
+ADD FOREIGN KEY (email) REFERENCES user_(email);
+ALTER TABLE guestinfo 
+ADD FOREIGN KEY (movie) REFERENCES movie(title);
+
+ALTER TABLE registerinfo 
+ADD FOREIGN KEY (email) REFERENCES user_(email);
+
+ALTER TABLE moviehistory 
+ADD FOREIGN KEY (username) REFERENCES registerinfo(username);
+ALTER TABLE moviehistory 
+ADD FOREIGN KEY (movie) REFERENCES movie(title);
+
+ALTER TABLE moviethread
+ADD FOREIGN KEY (user_) REFERENCES registerinfo(username);
+ALTER TABLE moviethread
+ADD FOREIGN KEY (movie) REFERENCES movie(title);
+
+ALTER TABLE moviethreadresponses
+ADD FOREIGN KEY (threadid) REFERENCES moviethread(id_);
+ALTER TABLE moviethreadresponses
+ADD FOREIGN KEY (user_) REFERENCES registerinfo(username);
+
+ALTER TABLE theatrethread
+ADD FOREIGN KEY (user_) REFERENCES registerinfo(username);
+ALTER TABLE theatrethread
+ADD FOREIGN KEY (theatre) REFERENCES theatre(address);
+
+ALTER TABLE theatreresponses
+ADD FOREIGN KEY (threadid) REFERENCES moviethread(id_);
+ALTER TABLE theatreresponses
+ADD FOREIGN KEY (user_) REFERENCES registerinfo(username);
+
+ALTER TABLE staff
+ADD FOREIGN KEY (hiredby) REFERENCES manager_(ssn);
+ALTER TABLE staff
+ADD FOREIGN KEY (location_) REFERENCES theatre(address);
+
+ALTER TABLE manager_
+ADD FOREIGN KEY (location_) REFERENCES theatre(address);
+
+ALTER TABLE schedule
+ADD FOREIGN KEY (staff) REFERENCES staff(ssn);
+ALTER TABLE schedule
+ADD FOREIGN KEY (type_) REFERENCES positions(type_);
+ALTER TABLE schedule
+ADD FOREIGN KEY (location_) REFERENCES theatre(address);
